6d272d533688c80d3e43d9e8a8809c583ed1bb4b
scmho-VB <scm.ho@v-bazaar.co.in>
2026-07-20 17:43:53 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-from app import routes
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
